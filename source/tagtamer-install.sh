4a10570a827924e8ecdca6a571c61d6373cb0b8b
Bill Dry <bdry@amazon.com>
2020-12-28 13:09:41 -0500

    Add cert bundle capability & correct naming to tag-tamer

--- source/tagtamer-install.sh
+++ source/tagtamer-install.sh
@@ -10,14 +10,14 @@ mkdir -p /home/ec2-user/tag-tamer/prod
 chown -R ec2-user:ec2-user /home/ec2-user/tag-tamer
 #pip3 install boto3 botocore flask flask-WTF gunicorn Flask_jwt_Extended flask_login
 #pip3 install /var/tmp/tag-tamer/source/Flask-AWSCognito
-su - ec2-user -c "python3 -m venv /home/ec2-user/tag-tamer/prod;source /home/ec2-user/tag-tamer/prod/bin/activate; pip3 install boto3 botocore flask flask-WTF gunicorn /var/tmp/tag-tamer/source/Flask-AWSCognito; deactivate"
+su - ec2-user -c "python3 -m venv /home/ec2-user/tag-tamer/prod;source /home/ec2-user/tag-tamer/prod/bin/activate; pip3 install boto3 botocore flask flask-WTF gunicorn python-jose /var/tmp/tag-tamer/source/Flask-AWSCognito; deactivate"
 
 # Copy code and config
 cd /var/tmp/tag-tamer/source
 cp config/tag-tamer.conf /etc/nginx/conf.d
 cp config/proxy_params /etc/nginx
 cp config/ssl-redirect.conf  /etc/nginx/default.d/
-cp config/tagtamer.service /etc/systemd/system
+cp config/tag-tamer.service /etc/systemd/system
 cp -pr code/* to /home/ec2-user/tag-tamer/
 
 mkdir -p /home/ec2-user/tag-tamer/log
@@ -26,19 +26,71 @@ touch /var/log/tag-tamer/tag-tamer.err.log
 touch /var/log/tag-tamer/tag-tamer.out.log
 
 # Permissions
-chown root:root /etc/nginx/conf.d/tag-tamer.conf /etc/nginx/proxy_params /etc/nginx/default.d/ssl-redirect.conf /etc/systemd/system/tagtamer.service
+chown root:root /etc/nginx/conf.d/tag-tamer.conf /etc/nginx/proxy_params /etc/nginx/default.d/ssl-redirect.conf /etc/systemd/system/tag-tamer.service
 chown -R ec2-user:ec2-user /home/ec2-user/tag-tamer /var/log/tag-tamer
 
 dos2unix /home/ec2-user/tag-tamer/*.py
 dos2unix /home/ec2-user/tag-tamer/templates/*.html
 
-# Fix IP in config
-sed -i  "s/192.168.10.80/`hostname -i`/g" /etc/nginx/conf.d/tag-tamer.conf 
+# set server_tokens to off in nginx.conf file
+sed -i '/server_tokens/d' /etc/nginx/nginx.conf
+sed -i '/sendfile.*/i\    server_tokens       off;' /etc/nginx/nginx.conf
 
-# SSL certificate creation
-mkdir -p /etc/pki/nginx/private 
-openssl req -x509 -nodes -days 398 -newkey rsa:2048 -keyout /etc/pki/nginx/private/nginx-selfsigned.key -out /etc/pki/nginx/nginx-selfsigned.crt -subj "/C=US/ST=NC/L=Raleigh/O=AWS/OU=TAM/CN=tagtamer.amazon.com"
-openssl dhparam -out /etc/pki/nginx/dhparam.pem 2048
+# SSL certificate creation - START
+
+# Fix IP in config
+sed -i  "s/10.0.5.59/`hostname -i`/g" /etc/nginx/conf.d/tag-tamer.conf 
+
+# Get Public or Private Hostnames/IPs to configure in certificate
+FQDN1=`curl http://169.254.169.254/latest/meta-data/local-hostname` 
+FQDN2=`curl http://169.254.169.254/latest/meta-data/public-hostname` 
+IP1=`curl http://169.254.169.254/latest/meta-data/local-ipv4`
+IP2=`curl http://169.254.169.254/latest/meta-data/public-ipv4`
+
+# Create root CA 
+mkdir -p /etc/pki/nginx/
+cd /etc/pki/nginx/
+openssl genrsa -out rootCA.key 2048
+openssl req -x509 -new -nodes -key rootCA.key -sha256 -days 398 -out rootCA.crt -subj "/C=US/ST=NC/L=Raleigh/O=AWS/OU=AWS Support/CN=amazonaws.com"
+
+# Create intermediate certificate - Alt DNS name input file
+echo "subjectAltName = @alt_names
+
+[alt_names]
+DNS.1 = $FQDN1
+IP.1 = $IP1" > v3.ext
+
+# Removed public IP/DNS from cert. Add below if needed to above section.
+# DNS.2 = $FQDN2
+# IP.2 = $IP2
+
+# Create intermediate certificate
+openssl genrsa -out tag-tamer.key 2048
+openssl req -new -sha256 -key tag-tamer.key -subj "/C=US/ST=NC/L=Raleigh/O=AWS/OU=AWS Support/CN=$FQDN1" -out tag-tamer.csr
+openssl x509 -req -in tag-tamer.csr -CA rootCA.crt -CAkey rootCA.key -CAcreateserial -out tag-tamer.crt -days 398 -sha256 -extfile  v3.ext  
+
+# Create file for import into browser, this can be shared to import into trust store of client from where Tag Tamer application is accessed.
+# ca-bundle file below.
+cat tag-tamer.crt rootCA.crt > tag-tamer.ca-bundle
+
+# Tag Tamer application connectivity from web browser will be shown as untrusted by default.
+# It's highly recommended to import ca-bundle into customer desktop from where Tag Tamer application is accessed.
+# Example: Import to Mac OS trust store
+# Ask application Administrator to import  /etc/pki/nginx/tag-tamer.ca-bundle to browser where required.
+# sudo security add-trusted-cert -d -r trustAsRoot -k /Library/Keychains/System.keychain tag-tamer.ca-bundle
+# For Windows certificate import process refer to
+# https://docs.microsoft.com/en-us/skype-sdk/sdn/articles/installing-the-trusted-root-certificate
+
+# Customer can also use their own trusted certificate instead self-signed certificate.
+# Update below 3 entries in /etc/nginx/conf.d/tag-tamer.conf with customer certificate path.
+#        ssl_certificate "/etc/pki/nginx/tag-tamer.crt";
+#        ssl_certificate_key "/etc/pki/nginx/tag-tamer.key";
+#	     ssl_client_certificate "/etc/pki/nginx/rootCA.crt";
+
+
+# Verification command on Tag Tamer web app server:  openssl x509 -text -noout -in tag-tamer.crt 
+
+# SSL certificate creation - END
 
 # Enable and start services
 systemctl enable tag-tamer.service; systemctl start tag-tamer.service
