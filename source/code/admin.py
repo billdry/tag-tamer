2b604e16f76be4daa9bdb2d28bc894a88cf75cac
Bill Dry <bdry@amazon.com>
2021-02-04 13:46:41 -0500

    Add function to assume role & return session object

--- source/code/admin.py
+++ source/code/admin.py
@@ -4,14 +4,30 @@
 # SPDX-License-Identifier: MIT-0
 # Tag Tamer administrative functions
 
+# Import AWS module for python
+import boto3
+from boto3.session import Session
+from botocore.exceptions import ClientError
+
 from time import time, gmtime, strftime
+
+# Import logging module
+import logging
+# Import the systems module to get interpreter data
+import sys
+
+log = logging.getLogger(__name__)
 
 # Return the date & current time
 def date_time_now():
     now = gmtime()
     time_string = strftime("%d-%B-%Y at %H:%M:%S UTC", now)
     return time_string
-  
+
+# Return the current epoch time (total number of seconds since January 1, 1970)
+#def epoch_time_now():
+#    now = gmtime()
+#    return now
 
 # Define execution_status class to return the execution status of Tag Tamer functions
 # alert_level variable aligns to getbootstrap_com/docs/4.5/components/alerts/
@@ -32,7 +48,7 @@ class execution_status:
             if kwargs.get('message'):
                 self.status['status_message'] = kwargs['message']
             else:
-                self.status['status_message'] = 'Please contact your Tag Tamer administrator.'
+                self.status['status_message'] = 'Please contact your Tag Tamer administrator for assistance.'
             self.status['alert_level'] = 'warning'
 
         def error(self, **kwargs):
@@ -44,4 +60,46 @@ class execution_status:
         
         def get_status(self):
             return self.status
+
+# Create & return a Boto3 session object for an IAM role assumed in another AWS account
+# Inputs: IAM role ARN, user email, user name & user source IP address or source hostname all as keyword arguments
+def assume_role_multi_account(**kwargs):
+    my_status = execution_status()
+    sts_client = boto3.client("sts")
+    if kwargs.get('user_id') and kwargs.get('user_email') and kwargs.get('user_source'):
+        session_name = kwargs.get('user_id') + '-tag-tamer-session-' + str(gmtime())
+    else:
+        log.error("Failed to invoke \"{}\" on {}".format(sys._getframe().f_code.co_name, date_time_now()))
+        my_status.error()
+        session_object = False
+        return session_object
+    
+    try:
+        response = dict()
+        response = sts_client.assume_role(
+            RoleArn=kwargs.get('role_arn'),
+            RoleSessionName=session_name
+        )
+        if len(response):
+            session_object = Session(
+                aws_access_key_id=response["Credentials"]["AccessKeyId"],
+                aws_secret_access_key=response["Credentials"]["SecretAccessKey"],
+                aws_session_token=response["Credentials"]["SessionToken"]
+                #region_name=region,
+            )
+            log.info("\"{}\" invoked \"{}\" on {} from location: \"{}\" using AWSAuth access key id: {} - SUCCESS".format(kwargs.get('user_email'), sys._getframe().f_code.co_name, date_time_now(), kwargs.get('user_source'), response["Credentials"]["AccessKeyId"]))
+            my_status.success(message='Assumed multi-account role & created user session!')
+        else:
+            log.error("\"{}\" invoked \"{}\" on {} from location: \"{}\" - FAILURE".format(kwargs.get('user_email'), sys._getframe().f_code.co_name, date_time_now(), kwargs.get('user_source')))
+            my_status.error()
+            session_object = False
+            return session_object
+
+    except ClientError as error:
+            log.error("Boto3 API returned error. function: {} - {}".format(sys._getframe().f_code.co_name, error))
+            my_status.error()
+            session_object = False
+
+    return session_object
+
             
